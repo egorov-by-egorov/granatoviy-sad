fb842bd9453e69abff7e48cd318966a49dda3d6f
smwebstudio <sm.programming@yandex.ru>
2019-10-26 15:08:21 +0400

    Create content-pure.php
    For text pages content (editable from control panel).

---
+++ template-parts/content-pure.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<?php
+		the_content();
+		
+		wp_link_pages( array(
+		'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pomegranate-garden-theme' ),
+		'after'  => '</div>',
+		) );
+	?>
+	
+</div>
