35773b87ea133c90db4b0f264635bf69913f9a2d
smwebstudio <sm.programming@yandex.ru>
2019-11-17 21:40:56 +0400

    Contact page changes
    Contact page changes

--- contact.php
+++ contact.php
@@ -21,6 +21,10 @@ Template Name: Контакты
                                     <img src="<?php bloginfo(template_url); ?>/assets/images/contact_icon--black.svg" alt="icon" class="contact__address-icon">
                                     <span>Чапаевский пер. 10/2</span>
                                 </li>
+								<li class="contact__address">
+                                    <p class="contact__address-subtitle"><b>График работы:</b></p>
+                                    <span>Пн-Вск: 11:00-00:00</span>
+                                </li>
                                 <li class="contact__address">
                                     <p class="contact__address-subtitle"><b>Бронь столов:</b></p>
                                     <a href="tel:+74952555333"></i>+7 (495) 2-555-333</a>
@@ -30,6 +34,7 @@ Template Name: Контакты
                                     <a href="tel:+74952555551"></i>+7 (495) 2-555-55-1</a>
                                 </li>
                                 <li class="contact__address">
+									<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
                                     <a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
                                 </li>
                             </ul>
@@ -42,6 +47,10 @@ Template Name: Контакты
                                 <li class="contact__address">
                                     <img src="<?php bloginfo(template_url); ?>/assets/images/contact_icon--black.svg" alt="icon" class="contact__address-icon">
                                     <span>ул.Поречная д. 5/14, стр. 1</span>
+                                </li>
+								<li class="contact__address">
+                                    <p class="contact__address-subtitle"><b>График работы:</b></p>
+                                    <span>Пн-Вск: 11:00-00:00</span>
                                 </li>
                                 <li class="contact__address">
                                     <p class="contact__address-subtitle"><b>Бронь столов:</b></p>
@@ -56,23 +65,12 @@ Template Name: Контакты
                                     <a href="tel:+74993472828">+7(499) 347-28-28</a>
                                 </li>
                                 <li class="contact__address">
+									<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
                                     <a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-lg-12 col-md-12 col-sm-12">
-                            <ul class="contact__address-list border-right-0">
-                                <li class="contact__address">
-                                    <h3 class="contact__address-title">Часы работы</h3>
-                                </li> 
-                                <li class="contact__address">
-                                    <span>Пн-Вск: 11:00-00:00</span>
-                                </li>
-                                <li class="contact__address">
-                                    <span>Бесплатная доставка по г.Москве</span>
-                                </li>
-                            </ul>
-                        </div>
+                       
                     </div>
                 </div>
             </section>
