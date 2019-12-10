e800d63d8080959f8763b787801e3037afcf9aeb
smwebstudio <sm.programming@yandex.ru>
2019-12-10 14:37:39 +0400

    Update header.php
    vk pixel added

--- header.php
+++ header.php
@@ -26,6 +26,8 @@
   src="https://www.facebook.com/tr?id=1012511649130642&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
+
+<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?162",t.onload=function(){VK.Retargeting.Init("VK-RTRG-435087-3Vgwd"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-435087-3Vgwd" style="position:fixed; left:-999px;" alt=""/></noscript>
 	</head>
 	
     <body>
