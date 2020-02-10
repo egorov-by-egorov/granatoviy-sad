b62396848ef7abd5494d75e5737a532440ec6c5d
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2020-02-10 13:09:00 +0300

    Обновил график работы ресторна на поречной.

--- contact.php
+++ contact.php
@@ -1,107 +1,112 @@
-<?php 
+<?php
 /*
 Template Name: Контакты
 */
 ?>
 <?php get_header() ?>
-    <section class="main__section banket">
-                <div class="container-fluid banket__header">
-                    <div class="container">
-                        <h2 class="banket__header-title">Контакты</h2>
-                    </div>  
-                </div>
-                <div class="container">
-                    <div class="row m-0">
-                        <div class="col-lg-4 col-md-4 col-sm-12 contact-page__address-list--1">
-                            <ul class="contact__address-list-1">
-                                <li class="contact__address">
-                                    <h3 class="contact__address-title">Ресторан на Чапаевском</h3>
-                                </li>    
-                                <li class="contact__address">
-                                     <p class="contact__address-subtitle"><b>Адрес:</b></p>
-                                    <span>Чапаевский пер. 10/2</span>
-                                </li>
-								<li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>График работы:</b></p>
-                                    <span>Пн-Вск: 11:00-00:00</span>
-                                </li>
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Бронь столов:</b></p>
-                                    <a href="tel:+74952555333"></i>+7 (495) 2-555-333</a>
-                                </li>
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Доставка еды:</b></p>
-                                    <a href="tel:+74952555551"></i>+7 (495) 2-555-55-1</a>
-                                </li>
-                                <li class="contact__address">
-									<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
-                                    <a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12">
-                            <ul class="contact__address-list-2">
-                                <li class="contact__address">
-                                    <h3 class="contact__address-title">Ресторан на Поречной</h3>
-                                </li>                                
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Адрес:</b></p>
-                                    <span>ул.Поречная д. 5/14, стр. 1</span>
-                                </li>
-								<li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>График работы:</b></p>
-                                    <span>Пн-Вск: 11:00-00:00</span>
-                                </li>
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Бронь столов:</b></p>
-                                    <a href="tel:+74993402552">+7(499) 340-25-52</a>
-                                </li>
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Доставка еды:</b></p>
-                                    <a href="tel:+74993400110">+7(499) 340-01-10</a>
-                                </li>
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Караоке NIGHT-CLUB:</b></p>
-                                    <a href="tel:+74993472828">+7(499) 347-28-28</a>
-                                </li>
-                                <li class="contact__address">
-									<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
-                                    <a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
-                                </li>
-                            </ul>
-                        </div>
-						
-						  <div class="col-lg-4 col-md-4 col-sm-12">
-                            <ul class="contact__address-list-2">
-                                <li class="contact__address">
-                                    <h3 class="contact__address-title">Гранат Холл</h3>
-                                </li>                                
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Адрес:</b></p>
-                                    <span>ул. Цимлянская 2а</span>
-                                </li>
-								<li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>График работы:</b></p>
-                                    <span>Пн-Вск: 11:00-00:00</span>
-                                </li>
-                                <li class="contact__address">
-                                    <p class="contact__address-subtitle"><b>Бронь столов:</b></p>
-                                    <a href="tel:+79252122222">+7 (925) 212-22-22</a>
-                                </li>
-                                
-                                <li class="contact__address">
-									<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
-                                    <a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
-                                </li>
-                            </ul>
-                        </div>
-                       
-                    </div>
-					
-					 <div class="row m-0">
-					 <p style="font-size:12px; margin-top:70px">
-					 ООО «Гранатовый сад №1» , ресторан Гранатовый сад №1. <br/>Юридический адрес - 141006, Московская область, г. Мытищи, ул. 1-ая Пролетарская, д. 6А. <br/>Фактический адрес - 125057, г. Москва, Чапаевский переулок, д. 10/2 . <br/>ОГРН - 1175029004590 .<br/>ИНН - 5029218270</p>
-					 </div>
-                </div>
-            </section>
+	<section class="main__section banket">
+		<div class="container-fluid banket__header">
+			<div class="container">
+				<h2 class="banket__header-title">Контакты</h2>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row m-0">
+				<div class="col-lg-4 col-md-4 col-sm-12 contact-page__address-list--1">
+					<ul class="contact__address-list-1">
+						<li class="contact__address">
+							<h3 class="contact__address-title">Ресторан на Чапаевском</h3>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Адрес:</b></p>
+							<span>Чапаевский пер. 10/2</span>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>График работы:</b></p>
+							<span>Пн-Вск: 11:00 - 00:00</span>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Бронь столов:</b></p>
+							<a href="tel:+74952555333"></i>+7 (495) 2-555-333</a>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Доставка еды:</b></p>
+							<a href="tel:+74952555551"></i>+7 (495) 2-555-55-1</a>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
+							<a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+					<ul class="contact__address-list-2">
+						<li class="contact__address">
+							<h3 class="contact__address-title">Ресторан на Поречной</h3>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Адрес:</b></p>
+							<span>ул.Поречная д. 5/14, стр. 1</span>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>График работы:</b></p>
+							<span>Пн-чт: 11:00 - 01:00</span><br>
+							<span>Пт-сб: 11:00 - 02:00</span><br>
+							<span>Вск: 11:00 - 01:00</span>
+
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Бронь столов:</b></p>
+							<a href="tel:+74993402552">+7(499) 340-25-52</a>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Доставка еды:</b></p>
+							<a href="tel:+74993400110">+7(499) 340-01-10</a>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Караоке NIGHT-CLUB:</b></p>
+							<a href="tel:+74993472828">+7(499) 347-28-28</a><br>
+							<p class="contact__address-subtitle"><b>График работы:</b></p>
+							<span>Пт-сб: 18:00 - 06:00</span>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
+							<a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-lg-4 col-md-4 col-sm-12">
+					<ul class="contact__address-list-2">
+						<li class="contact__address">
+							<h3 class="contact__address-title">Гранат Холл</h3>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Адрес:</b></p>
+							<span>ул. Цимлянская 2а</span>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>График работы:</b></p>
+							<span>Пн-Вск: 11:00 - 00:00</span>
+						</li>
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Бронь столов:</b></p>
+							<a href="tel:+79252122222">+7 (925) 212-22-22</a>
+						</li>
+
+						<li class="contact__address">
+							<p class="contact__address-subtitle"><b>Эл. адрес:</b></p>
+							<a href="mailto:info@granatoviy-sad.ru">info@granatoviy-sad.ru</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+
+			<div class="row m-0">
+				<p style="font-size:12px; margin-top:70px">
+					ООО «Гранатовый сад №1» , ресторан Гранатовый сад №1. <br/>Юридический адрес - 141006, Московская область, г. Мытищи, ул. 1-ая Пролетарская, д. 6А. <br/>Фактический адрес - 125057, г. Москва, Чапаевский переулок, д. 10/2 . <br/>ОГРН - 1175029004590 .<br/>ИНН - 5029218270</p>
+			</div>
+		</div>
+	</section>
 <?php get_footer(); ?>
